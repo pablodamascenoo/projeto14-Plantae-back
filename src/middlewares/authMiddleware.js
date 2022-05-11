13bf8f83431221c742549b78d0b86550f5671045
pablodamascenoo <pablofacex@gmail.com>
2022-05-11 16:00:20 -0300

    feat: criado o middleware de login

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-import { schemaCadastro } from "../schemas/authSchemas.js";
+import { schemaCadastro, schemaLogin } from "../schemas/authSchemas.js";
 
 export async function validarCadastro(req, res, next) {
   const { nome, email, senha, senha2 } = req.body;
@@ -15,5 +15,18 @@ export async function validarCadastro(req, res, next) {
   }
 
   res.locals.cadastro = value;
+  next();
+}
+
+export async function validaLogin(req, res, next) {
+  const { email, senha } = req.body;
+
+  const { error, value } = schemaLogin.validate({ email, senha });
+
+  if (error) {
+    res.status(422).send(error.details[0].message);
+  }
+
+  res.locals.login = value;
   next();
 }
