8f2d3d748f913fc4860d627b18d1796d82561ddd
pablodamascenoo <pablofacex@gmail.com>
2022-05-11 16:00:45 -0300

    feat: criada a rota de login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { cadastro } from "../controllers/authController.js";
-import { validarCadastro } from "../middlewares/authMiddleware.js";
+import { cadastro, login } from "../controllers/authController.js";
+import { validaLogin, validarCadastro } from "../middlewares/authMiddleware.js";
 
 const authRouter = Router();
 
 authRouter.post("/cadastro", validarCadastro, cadastro);
+authRouter.post("/login", validaLogin, login);
 
 export default authRouter;
