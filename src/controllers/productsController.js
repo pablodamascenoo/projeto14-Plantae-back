d2c5651ee7cb39778d285fa66b177344326a1bc3
pablodamascenoo <pablofacex@gmail.com>
2022-05-12 17:30:00 -0300

    feat: adicionada a função paginaProduto

---
+++ src/controllers/productsController.js
@@ -0,0 +1,22 @@
+import db from "./../db.js";
+import { perigo } from "../misc/consoleColorido.js";
+import { ObjectID } from "bson";
+
+export async function PaginaProduto(req, res) {
+  const { id } = req.params;
+
+  if (id.length < 24) return res.sendStatus(404);
+
+  try {
+    const produto = await db
+      .collection("produtos")
+      .findOne({ _id: ObjectID(id) });
+    if (!produto) return res.sendStatus(404);
+
+    return res.status(200).send(produto);
+  } catch (erro) {
+    res.status(500).send("erro interno no servidor, tente novamente!");
+    perigo(erro);
+    return;
+  }
+}
