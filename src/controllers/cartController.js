1a9d114213bd16f46159580cf2d5f6f6200b9b74
pablodamascenoo <pablofacex@gmail.com>
2022-05-16 15:34:08 -0300

    fix: arrumado erro do get carrinho

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -17,7 +17,7 @@ export async function postCarrinho(req, res) {
 
     if (produto) {
       let quantidadeNova = produto;
-      quantidadeNova = (quantidadeNova * 1 + quantidade).toString();
+      quantidadeNova = quantidadeNova * 1 + quantidade;
 
       await db
         .collection("usuarios")
@@ -45,28 +45,26 @@ export async function postCarrinho(req, res) {
 }
 
 export async function getCarrinho(req, res) {
-
   const { usuario } = res.locals;
-  const {carrinho} = usuario;
-  
+  const { carrinho } = usuario;
+
   try {
-    let produtos = await db.collection("produtos").find().toArray();
-    
-    if(carrinho.length === 0){
+    let produtos = await db.collection("produtos").find({}).toArray();
+
+    if (carrinho.length === 0) {
       return res.status(401).send("O carrinho está vazio");
     }
-    produtos.map(produto => {
-      carrinho.map(item => {
-        if(item.idProduto === produto._id){
-          console.log("Achei um item");
-        }
+    const itensCarrinho = carrinho.map((item) => {
+      const produto = produtos.find((produto) => {
+        return `${produto._id}` === item.idProduto ? true : false;
       });
+      return { ...produto, quantidade: item.quantidade };
     });
 
-    res.status(200).send(carrinho);
+    res.status(200).send(itensCarrinho);
   } catch (erro) {
     perigo(erro);
-    console.log("Erro na requisição")
+    console.log("Erro na requisição");
     res.status(500).send(erro);
   }
 }
