08871c50e727e7d100b30490e25cc965435f3afa
pablodamascenoo <pablofacex@gmail.com>
2022-05-15 21:06:02 -0300

    feat: schemas de endereço e pagamento criados

---
+++ src/schemas/checkoutSchemas.js
@@ -0,0 +1,30 @@
+import Joi from "joi";
+
+export const schemaEndereco = Joi.object({
+  nome: Joi.string().required(),
+  cep: Joi.string()
+    .pattern(/^\d{8}$/)
+    .required(),
+  logradouro: Joi.string().required(),
+  bairro: Joi.string().required(),
+  cidade: Joi.string().required(),
+  estado: Joi.string()
+    .pattern(/^\w{2}$/)
+    .required(),
+  complemento: Joi.string().required(),
+});
+
+export const schemaPagamento = Joi.object({
+  numero: Joi.string()
+    .pattern(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})$/)
+    .required(),
+  cpf: Joi.string()
+    .pattern(/^(\d{3}).(\d{3}).(\d{3})-(\d{2})$/)
+    .required(),
+  codigo: Joi.string()
+    .pattern(/^(\d{4})$/)
+    .required(),
+  validade: Joi.string()
+    .pattern(/^(\d{4})-(((0)[1-9])|(1)[0-2])$/)
+    .required(),
+});
